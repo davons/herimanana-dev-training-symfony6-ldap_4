646bcd9f40e5f125fe4b1084c89bd76189fe43bb
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2024-06-03 23:01:49 +0200

    LDAP avec Symfony

---
+++ src/Security/AppCustomAuthenticator.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Security;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Ldap\Ldap;
+use Symfony\Component\Ldap\LdapInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(
+        private readonly Ldap $ldap,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private $baseDn,
+        private $searchDn,
+        private $searchPassword
+    ){
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->getPayload()->getString('email');
+        $password = $request->getPayload()->getString('password');
+
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+
+        /*$this->ldap->bind($this->searchDn, $this->searchPassword);
+        $username = $this->ldap->escape($email, '', LdapInterface::ESCAPE_FILTER);
+        $param = str_replace('{username}', $username, $this->searchDn);
+        $query = $this->ldap->query($this->baseDn, $param);*/
+
+        $this->ldap->bind("uid={$email},dc=example,dc=com", $password);
+        $query = $this->ldap->query($this->baseDn, "(uid={$email})");
+        $results = $query->execute();
+        $entry = $results[0];
+
+        $username = $entry->getAttribute('uid')[0];
+        $fullName = $entry->getAttribute('cn')[0];
+        $lastName = $entry->getAttribute('sn')[0];
+        $email = $entry->getAttribute('mail')[0];
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+                new RememberMeBadge(),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        // For example:
+        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
